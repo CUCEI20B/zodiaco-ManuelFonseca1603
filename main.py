cc6a6ea19c70fdcffb2b7b9f8dd1bbe0774e5941
Manuel Fonseca <manuel.fonseca45499@alumnos.udg.mx>
2020-10-02 10:26:34 -0500

    prueba_03

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ dia = int(input("Escribe el dia: "))
 mes = int(input("Escribe el mes: "))
 
 if dia >= 20 and mes == 1 or dia <= 18 and mes == 2:
-    print("Eres Acuario")
+    print("acuario")
 elif  dia >= 19 and mes == 2 or dia <= 20 and mes == 3:
-    print("Eres Piscis")
+    print("piscis")
 elif dia >= 21 and mes == 3 or dia <= 19 and mes == 4:
-    print("Eres Aries")
+    print("aries")
 elif dia >= 20 and mes == 4 or dia <= 20 and mes == 5:
-    print("Eres Tauro")
+    print("tauro")
 elif dia >= 21 and mes == 5 or dia <=20 and mes == 6:
-    print("Eres Geminis")
+    print("geminis")
 elif dia >= 21 and mes == 6 or dia <= 22 and mes == 7:
-    print("Eres Cancer")
+    print("cancer")
 elif dia >= 23 and mes == 7 or dia <= 22 and mes == 8:
-    print("Eres Leo")
+    print("leo")
 elif dia >= 23 and mes == 8 or dia <= 22 and mes == 9:
-    print("Eres Virgo")
+    print("virgo")
 elif dia >= 23 and mes == 9 or dia <= 22 and mes == 10:
-    print("Eres Libra")
+    print("libra")
 elif dia >= 23 and mes == 10 or dia <= 21 and mes == 11:
-    print("Eres Escorpio")
+    print("escorpio")
 elif dia >= 22 and mes == 11 or dia <= 21 and mes == 12:
-    print("Eres Sagitario")
+    print("sagitario")
 else:
-    print("Eres Capricornio")
+    print("capricornio")
